8dc1ebd72ec2b2c0ace45d2a8a80f0372413a7eb
Mark Strancheuski <mariknowhere@gmail.com>
2022-10-17 17:31:33 +0300

    C-4: Added navigate to MainPage in Header

--- src/blocks/header/Header.tsx
+++ src/blocks/header/Header.tsx
@@ -1,10 +1,16 @@
 import React, {FC} from 'react';
 import {TITLE} from "../../constant/mainPage";
+import {useNavigate} from "react-router-dom";
+import {MAIN_ROUTE} from "../../constant/routes";
 
 const Header: FC = () => {
+  const navigate = useNavigate();
+
+  const onTitleClick = () => navigate(MAIN_ROUTE);
+
   return (
     <header>
-      <h1 className="text-center cursor-pointer font-bold text-5xl text-gray-500 pt-6 hover:text-green-500">
+      <h1 onClick={onTitleClick} className="text-center cursor-pointer font-bold text-5xl text-gray-500 pt-6 hover:text-green-500">
         {TITLE}
       </h1>
     </header>
